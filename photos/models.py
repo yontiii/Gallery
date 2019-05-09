adc3945a18dc77fe44cd26b80a27f967b847a1be
yontey <muasajohn01@gmail.com>
2019-05-09 12:12:31 +0300

    created Image model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField(max_length=50)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    date_taken = mo
+    
+    def __str__(self):
+        return self.image_name
+
+class Location(models.Model:
+
+    
+    
+    
