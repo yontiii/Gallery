0bb3559f1d3d48c382764d31ccbde6964d845bd0
yontey <muasajohn01@gmail.com>
2019-05-10 10:54:40 +0300

    added first image

--- photos/models.py
+++ photos/models.py
@@ -8,10 +8,11 @@ class Location(models.Model):
     
     def __str__(self):
         return self.location
-
+    
 class Category(models.Model):
     
     category = models.CharField(max_length =20)
+    
     
     def __str__(self):
         return self.category
@@ -20,7 +21,7 @@ class Category(models.Model):
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     image_name = models.CharField(max_length =30)
-    image_description = models.TextField(max_length=50)
+    image_description = models.TextField(max_length=5000)
     date_taken = models.DateTimeField(auto_now_add=True)
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
@@ -33,6 +34,8 @@ class Image(models.Model):
     
     def __str__(self):
         return self.image_name
+    
+
     
    
     
