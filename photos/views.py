f760464b8078ef85f92acc519ea78e4ec819eb5d
yontey <muasajohn01@gmail.com>
2019-05-10 13:06:25 +0300

    displayed initial images using fancybox

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image,Category,Location
 
 # Create your views here.
 
 def welcome(request):
-    return render(request, 'all-photos/index.html')
+    images = Image.objects.all()
+    return render(request, 'all-photos/index.html',{"images":images,})
