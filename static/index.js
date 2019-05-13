5dcd11f1a4ebb93b8e395d1ba63f60faa1c53d33
yontey <muasajohn01@gmail.com>
2019-05-13 10:41:31 +0300

    Added Image modal

--- static/index.js
+++ static/index.js
@@ -1,13 +1,7 @@
 
-$(document).ready(function(){
-    $(function () {
-    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-    });
 
- copyimage = () => {
-    var copyLink = document.getElementById("imagelink")
-    copyLink.select()
+ function copyLink() {
+    var copyLink = document.getElementById("linkedimage");
+    copyLink.select();
     document.execCommand("copy")
-    
 } 
-})
